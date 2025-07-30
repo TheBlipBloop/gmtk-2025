0161fa7c09951c7827e0836e961a334b14210e9b
TheBlipBloop <blipbloopdev@gmail.com>
2025-07-30 16:40:21 -0700

    add camera mouse helper, tweaking ship movement

---
+++ scripts/camera_mouse_helper.gd
@@ -0,0 +1,9 @@
+extends Camera3D
+class_name CameraMouseHelper
+
+static var mouse_position : Vector2
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var a = project_position(get_viewport().get_mouse_position(), 1.0)
+	mouse_position = Vector2(a.x, a.y)
--- scripts/ship_movement.gd
+++ scripts/ship_movement.gd
@@ -2,6 +2,8 @@ extends RigidBody3D
 
 class_name Movement
 
+@export var camera : Camera3D;
+
 @export var base_slow_down_speed : float = 0.2;
 
 # Base movement speeds
@@ -12,7 +14,7 @@ class_name Movement
 @export var boost_rotation_speed : float = 2.5;
 @export var boost_move_speed : float = 1.75;
 
-@export var loop_boost_speed : float = 5.0;
+@export var loop_boost_speed : float = 3.0;
 
 @export var looper_component : LoopDetector;
 
@@ -27,6 +29,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	m_input_rotation = 0
+	
+	#var positionflat = Vector2(global_position.x, global_position.z)
+	#m_input_rotation = CameraMouseHelper.mouse_position.angle_to(positionflat)
+	#m_input_rotation = clampf(m_input_rotation, -1, 1)
 	if Input.is_action_pressed("rotate_cw"):
 		m_input_rotation += 1
 	if Input.is_action_pressed("rotate_ccw"):
@@ -44,12 +50,17 @@ func _process(delta: float) -> void:
 		_boost()
 
 func _boost():
-	linear_velocity = basis.z * loop_boost_speed
+	linear_velocity += basis.z * loop_boost_speed
 	angular_velocity = Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
 	angular_velocity += Vector3.UP * m_input_rotation * -_get_rotation_speed() * delta;
 	linear_velocity += basis.z * _get_move_speed() * delta;
+	
+	# drift force
+	if linear_velocity.dot(basis.z) < 0.0 and m_input_boosting:
+		var force = max(_get_move_speed(), linear_velocity.length())
+		linear_velocity += basis.z * force * delta * 1.1;
 	
 	if not m_input_boosting:
 		linear_velocity = linear_velocity.move_toward(Vector3.ZERO, delta * base_slow_down_speed);
